2e8e2d036bc7190f795eb740751a4583ec4247e7
mykolaievych <72654463+mykolaievych@users.noreply.github.com>
2021-09-25 14:55:45 +0300

    added miniletters, width to letters column

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Card from '../card/Card'
+import MiniLetter from '../letter/MiniLetter'
 import './home.scss'
 export default function Home(){
     return(<div className='home'>
         <div className='letters_and_graffic'>
            
-        <Card class='fixed_component'>
-            5 листів
+        <Card class='fixed_component letters_column'>
+           <MiniLetter nameAndSurname={'letter'} topic={'topic'}/>
+           <MiniLetter nameAndSurname={'letter'} topic={'topic'}/>
+           <MiniLetter nameAndSurname={'letter'} topic={'topic'}/>
+           <MiniLetter nameAndSurname={'letter'} topic={'topic'}/>
+           <MiniLetter nameAndSurname={'letter'} topic={'topic'}/>
         </Card>
+        
         
      
         <Card class='max_component'>
